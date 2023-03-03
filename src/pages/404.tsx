62bdd4088ad4e649b13b1e697d02499e846d5164
carrotpieOwO <xxhayoxx@naver.com>
2023-03-04 01:35:01 +0900

    404페이지 커스텀

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,25 @@
 import * as React from "react"
 import { Link, HeadFC, PageProps } from "gatsby"
+import ILayout from "../components/ILayout"
+import Seo from "../components/Seo"
+import { Typography } from 'antd';
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const { Title, Text } = Typography;
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
+    <ILayout>
+      <Title>Page not found</Title>
+        <Text>
         Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
+        </Text>
+        <br/>  
         <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    </ILayout>
   )
 }
 
 export default NotFoundPage
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <Seo title="ha0peno | 404 ❤️"/>
