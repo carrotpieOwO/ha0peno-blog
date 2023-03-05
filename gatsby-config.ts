8a6f9334c82cbbdde826709b834c2b60ef17af32
carrotpieOwO <xxhayoxx@naver.com>
2023-03-05 15:02:45 +0900

    rss 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -41,6 +41,59 @@ const config: GatsbyConfig = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.nodes.map(blog => {
+                return Object.assign({}, blog.frontmatter, {
+                  description: blog.excerpt,
+                  date: blog.frontmatter.date,
+                  title: blog.frontmatter.title,
+                  url: site.siteMetadata.siteUrl + blog.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + blog.frontmatter.slug,
+                  custom_elements: [{ 'content:encoded': blog.body }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(
+                  sort: { frontmatter: {date: DESC} }
+                ) {
+                  nodes {
+                    excerpt
+                    body
+                    frontmatter {
+                      title
+                      date
+                      slug
+                      category
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: "ha0peno's blog",
+          },
+        ],
+      },
+    },
   ],
 }
 
