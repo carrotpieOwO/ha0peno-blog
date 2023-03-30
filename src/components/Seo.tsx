8a0b0e437d9f05bbae2b2b54e5445eb2d5b36ce5
carrotpieOwO <xxhayoxx@naver.com>
2023-03-30 17:05:51 +0900

    naver meta tag등록

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -11,6 +11,7 @@ export default function Seo({title}: ISeoProps) {
             <Helmet>
                 <meta name="description" content="ha0peno 기술블로그" />
                 <meta name="google-site-verification" content="V4Y8kIq_sMSh2nSAFuhSMq5TOfEtj0xIUc-n5Gl3CA8" />
+                <meta name="naver-site-verification" content="8da7a8fd92d108358eb08f1b07fcc416ad01b0af" />
             </Helmet>
             <title>{title}</title>
         </>
